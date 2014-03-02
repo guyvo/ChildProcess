c538b4fc87d3be7f7d7242a1f275d575f64c5e47
Guy Van Overtveldt <guyvanovertveldt@Guys-MacBook-Air.local>
2014-03-02 15:33:27 +0100

    Initial Commit

---
+++ ChildProcess/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ChildProcess
+//
+//  Created by Guy Van Overtveldt on 02/03/14.
+//  Copyright (c) 2014 Guy Van Overtveldt. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
